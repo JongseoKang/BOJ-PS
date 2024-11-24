48eeb2206ad714a9fecad26ebe35d4dde53685af
JongseoKang <117558165+JongseoKang@users.noreply.github.com>
2024-11-24 13:57:27 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int T;
+	char str[1001];
+
+    scanf("%d", &T);
+	while(T--){
+		scanf("%s ", str);
+		int len = strlen(str);
+		printf("%c%c\n", str[0], str[len-1]);
+	}
+	return 0;
+}
